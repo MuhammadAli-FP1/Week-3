6f286d17d75e077d8f046d41e54f308076270fcb
ml.qodirov <ml.qodirov@newuu.uz>
2025-01-30 15:29:14 +0500

    Week 3

---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+// int main() {
+//     int a,b,c,d,e;
+//     cin>>a>>b>>c>>d>>e;
+//     int sum=a+b+c+d+e;
+//     cout<<"Sum is "<<sum<<endl;
+//     return 0;
+// }
+// int main() {
+//     int student_ID, student_GPA, student_Year_of_Study,student_Study_fee;
+//     cout<<"  Student ID: ";
+//     cin>>student_ID;
+//     cout<<"  Student GPA: ";
+//     cin>>student_GPA;
+//     cout<<"  Student Year of Study: ";
+//     cin>>student_Year_of_Study;
+//     cout<<"  Student Study fee: ";
+//     cin>> student_Study_fee;
+//     cout<< "Student ID: "<<student_ID<<endl;
+//     cout<<"Student GPA: "<<student_GPA<<endl;
+//     cout<<"Student Year of Study: "<<student_Year_of_Study<<endl;
+//     cout<<"Student Study fee: "<<student_Study_fee<<endl;
+//
+//     return 0;
+// }
+// int main() {
+//     float celcius_temperatura, fahrenheit_celcius;
+//     cout << "Please enter the temperatura in Celcius: ";
+//     cin >> celcius_temperatura;
+//     fahrenheit_celcius = (celcius_temperatura - 32) / 1.8;
+//     cout<<"Temperature in Celcius: "<<celcius_temperatura<<endl;
+//     cout<<"The Temperature in Fahrenheit: "<<fahrenheit_celcius<<endl;
+//     return 0;
+//
+// int main() {
+//     double cost_for_one_item;
+//     int quantity;
+//     cout << "Enter cost for one item: ";
+//     cin>>cost_for_one_item;
+//     cout<<"Please inter the quantity: ";
+//     cin>>quantity;
+//     double cost_per_item = cost_for_one_item * quantity;
+//     cout<<"Total cost is "<<cost_per_item<<endl;
+//     return 0;
+
+
+
+
+//5-problem
+// int main() {
+//     int age1, age2, age3, age4;
+//     float average_age_of_family;
+//     cout<<"Enter your dad's age: ";
+//     cin>>age1;
+//     cout<<"Enter your mum's age: ";
+//     cin>>age2;
+//     cout<<"Enter your sister's age: ";
+//     cin>>age3;
+//     cout<<" Inter your age: ";
+//     cin>>age4;
+//     average_age_of_family = (age1 + age2 + age3 + age4)/4.0;
+//     cout<<"Average age of family: "<<average_age_of_family<<endl;
+//     return 0;
+// }
+//6-problem
+
+
+
+// int main() {
+//     int length_a, length_b, perimetr, area;
+//     cout << "Inter legth a= " ;
+//     cin>>length_a;
+//     cout<<"Inter legth b= ";
+//     cin>>length_b;
+//     perimetr = (length_a+length_b)*2;
+//     area= length_a*length_b;
+//     cout<<"The area of the rectangle= "<< area<<endl;
+//        cout<<"The perimeter of the rectangle :" << perimetr<<endl;
+//     return 0;
+// }
+// 7-problem
+// int main() {
+//     int dividend, divisor, quotient;
+//     cout << "Enter dividend: ";
+//     cin >> dividend;
+//     cout << "Enter divisor: ";
+//     cin >> divisor;
+//     quotient = dividend / divisor;
+//     dividend = dividend % divisor;
+//     cout << "The quotient of the division is: " << quotient<< "\n "
+//     "The remainder of the division is: " << dividend << endl;
+//
+//
+//     return 0;
+//
+// }
