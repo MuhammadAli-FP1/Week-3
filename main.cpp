6ad08f9daacb8febbf9dd48f7c7eb17ff3f05e0c
Spin <ml.qodirov@newuu.uz>
2025-02-06 22:46:16 +0500

    Week 4

--- main.cpp
+++ main.cpp
@@ -104,11 +104,174 @@
 //
 //  }
 // }
-//9-problem
-int main() {
- cout<<"Inter the colour of traffic lights: ";
- char traffic_lights;
- if(traffic_lights
+// //9-problem
+// int main() {
+//  cout<<"Inter the colour of traffic lights: ";
+//  char traffic_lights;
+//  cin>>traffic_lights;
+//  if(traffic_lights== 'g')
+//  {
+//   cout<<"Go!";
+//  } else if (traffic_lights=='y') {
+//   cout<<"Get ready";
+//  } else if (traffic_lights=='r')
+//   cout<<"Stop";
+//  else
+//   {
+//   cout<<"Invalid input";
+//   return 0;
+//  }
+// }
+// 10-problem
+// int main() {
+//  cout << "Inter your grade: ";
+//  int grade;
+//  cin >> grade;
+//  if (grade >= 90 && grade <= 100) {
+//   cout<<"A";
+//  }
+//  else if (grade >= 80 && grade <= 89) {
+//   cout<<"B";
+//  }
+//  else if (grade >= 70 && grade <= 79) {
+//   cout<<"C";
+//  }
+//  else if (grade >= 60 && grade <= 69) {
+//   cout<<"D";
+//  }
+//  else
+//  {
+//   cout<<"F";
+//  }
+//
+//
+//   return 0;
+//  }
+// // 11- problem
+// int main() {
+//  cout<<"Inter the two number: ";
+//  int n1,n2;
+//  cin>>n1>>n2;
+//  if(n1%n2==0){cout<<n1<<" is divisible by "<<n2;
+//      }
+//     else {
+//         cout<<n1<<" is not divisible by"<<n2;
+//     }
+//      return 0;
+// }
+//12-problem
+// int main() {
+//  cout<<" Inter the three number:  ";
+//  int n1,n2,n3;
+//  cin>>n1>>n2>>n3;
+//  if((n1+n2)>n3 && ((n2+n3)>n1) && ((n1+n3)>n2))
+//   cout<<"The triangle is valid";
+//   else
+//   cout<<"The triangle is invalid";
+//   return 0;
+//
+// }
+// //13-problem
+// int main() {
+//  cout<<" Inter your letter :  ";
+//  char letter;
+//  cin>>letter;
+//  if ( 'A'>= letter && letter <='Z') {
+//   cout<<" Uppercase alphabet";
+//  }
+//  else if('a'<=letter && letter <='z') {
+//   cout<<"Lowercase alphabet";
+//  }
+//  else {
+//   cout<<"It is not aplhabet";
+//  }
+//  return 0;
+// }
+//14-problem
+// int main() {
+//  double weight1, price1, weight2, price2;
+//  cout << "Enter weight (kg) and price ($) of first package: ";
+//  cin >> weight1 >> price1;
+//  cout << "Enter weight (kg) and price ($) of second package: ";
+//  cin >> weight2 >> price2;
+//  double costPerKg1 = price1 / weight1;
+//  double costPerKg2 = price2 / weight2;
+//  cout << "\nCost per kg of first package: $" << costPerKg1 << endl;
+//  cout << "Cost per kg of second package: $" << costPerKg2 << endl;
+//
+//  if (costPerKg1 < costPerKg2) {
+//   cout << "The first package offers the better price." << endl;
+//  } else if (costPerKg2 < costPerKg1) {
+//   cout << "The second package offers the better price." << endl;
+//  } else {
+//   cout << "Both packages have the same price per kg." << endl;
+//  }
+//
+//  return 0;
+// }
+// //15-problem
+// int main() {
+//  int number;
+//  cout << "Enter a three-digit integer: ";
+//  cin >> number;
+//  int firstDigit = number / 100;
+//  int lastDigit = number % 10;
+//  if (firstDigit == lastDigit) {
+//   cout << number << " is a palindrome number" ;
+//  } else {
+//   cout << number << " is not a palindrome number." << endl;
+//  }
+//
+//  return 0;
+// }
+// 16-problem
+#include <cmath>
+// int main() {
+//  double x, y;
+//  const double radius = 10.0;
+//  cout << "Enter the x and y coordinates of the point: ";
+//  cin >> x >> y;
+//  double distance = sqrt(pow(x, 2) + pow(y, 2));
+//  if (distance <= radius) {
+//   cout << "The point (" << x << ", " << y << ") is inside the circle.";
+//  } else {
+//   cout << "The point (" << x << ", " << y << ") is outside the circle." ;
+//  }
+//
+//  return 0;
+// }
+// //17-problem
+//
+// int main() {
+//  double gpa;
+//  cout << "Enter your GPA (0 - 4.5): ";
+//  cin >> gpa;
+//  int gpaScale = static_cast<int>(gpa * 10);
+//  switch (gpaScale) {
+//   case 40: case 41: case 42: case 43: case 44: case 45:
+//       cout << "You are eligible for an 80% scholarship!" << endl;
+//   break;
+//   case 35: case 36: case 37: case 38: case 39:
+//       cout << "You are eligible for a 60% scholarship!" << endl;
+//   break;
+//   case 30: case 31: case 32: case 33: case 34:
+//       cout << "You are eligible for a 50% scholarship!" << endl;
+//   break;
+//   default:
+//    cout << "Sorry, no scholarship is available for GPA below 3.0." << endl;
+//  }
+//
+//  return 0;
+// }
 
- }
-}
+
+
+
+
+
+
+
+
+
+
+
